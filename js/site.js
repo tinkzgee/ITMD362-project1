80efa6d75ae7f18b96d13c73651c654041c60091
tinkzgee <35548208+tinkzgee@users.noreply.github.com>
2019-03-06 23:00:07 -0600

    changed submit event listener

--- js/site.js
+++ js/site.js
@@ -3,6 +3,25 @@ jQuery( function( $ ) {
   $( 'html' ).removeClass( 'nojs' );
   $( 'html' ).addClass( 'hasjs' );
 } );
-document.getElementById( "submit" ).addEventListener( "click", function() {
-  alert( "Email Recieved: Watch your brand grow!" );
-} );
+document.getElementById("submit").addEventListener("click", confirm);
+
+function confirm(event){
+  for(var i = 0; i < document.getElementsByTagName("input").length; i++)
+  {
+    if(document.getElementsByTagName("input").item(i).value === "" ||
+        (document.getElementsByTagName("input").item(i).type === "radio") &&
+        (document.getElementsByTagName("input").item(i).checked === false))
+        {
+          return false;
+        } 
+        
+        else 
+        {
+          name = document.getElementById("name").value;
+          
+          alert("Thank you " + name + "," + " email recieved!\n" +
+                "Watch your brand grow!");
+          return true;
+        }
+  }
+}
