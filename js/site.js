89ff6db1d4148cf5a9fed0acb7700633746fe8d9
tinkzgee <35548208+tinkzgee@users.noreply.github.com>
2019-03-06 23:28:53 -0600

    fixed js indentation

--- js/site.js
+++ js/site.js
@@ -1,27 +1,18 @@
 // jQuery 3.x-style ready event and locally scoped $
-jQuery( function( $ ) {
-  $( 'html' ).removeClass( 'nojs' );
-  $( 'html' ).addClass( 'hasjs' );
-} );
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
 document.getElementById("submit").addEventListener("click", confirm);
 
-function confirm(event){
-  for(var i = 0; i < document.getElementsByTagName("input").length; i++)
-  {
-    if(document.getElementsByTagName("input").item(i).value === "" ||
-        (document.getElementsByTagName("input").item(i).type === "radio") &&
-        (document.getElementsByTagName("input").item(i).checked === false))
-        {
-          return false;
-        } 
-        
-        else 
-        {
-          name = document.getElementById("name").value;
-          
-          alert("Thank you " + name + "," + " email recieved!\n" +
-                "Watch your brand grow!");
-          return true;
-        }
+function confirm(event) {
+  for (var i = 0; i < document.getElementsByTagName("input").length; i++) {
+    if (document.getElementsByTagName("input").item(i).value === "" || (document.getElementsByTagName("input").item(i).type === "radio") && (document.getElementsByTagName("input").item(i).checked === false)) {
+      return false;
+    } else {
+      name = document.getElementById("name").value;
+      alert("Thank you " + name + "," + " email recieved!\n" + "Watch your brand grow!");
+      return true;
+    }
   }
 }
